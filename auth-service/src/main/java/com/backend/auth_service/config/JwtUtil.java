1ddb3620c8adc37f12ae39333768bb7cdbc040d1
Nguyen Tung Duong <nguyentungduonglk1@gmail.com>
2026-04-27 19:43:33 +0700

    config auth

--- auth-service/src/main/java/com/backend/auth_service/config/JwtUtil.java
+++ auth-service/src/main/java/com/backend/auth_service/config/JwtUtil.java
@@ -2,22 +2,20 @@ package com.backend.auth_service.config;
 
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;
 import org.springframework.stereotype.Component;
 
+import javax.crypto.SecretKey;
 import java.util.Date;
 
 @Component
 public class JwtUtil {
-    private final String SECRET = "secret123";
-
+    private final SecretKey key = Keys.secretKeyFor(io.jsonwebtoken.SignatureAlgorithm.HS256);
     public String generateToken(String username, String role){
 
         return Jwts.builder()
                 .setSubject(username)
-                .claim("role",role)
-                .setIssuedAt(new Date())
-                .setExpiration(new Date(System.currentTimeMillis() + 86400000))
-                .signWith(SignatureAlgorithm.HS256,SECRET)
+                .signWith(key)
                 .compact();
     }
 }
--- auth-service/src/main/java/com/backend/auth_service/entity/Role.java
+++ auth-service/src/main/java/com/backend/auth_service/entity/Role.java
@@ -2,6 +2,7 @@ package com.backend.auth_service.entity;
 
 import jakarta.persistence.*;
 import lombok.Data;
+import lombok.ToString;
 
 import java.util.Set;
 
@@ -17,6 +18,7 @@ public class Role {
     private String name;
 
     @ManyToMany(mappedBy = "roles")
+    @ToString.Exclude // QUAN TRỌNG: Loại bỏ trường này khỏi toString()
     private Set<User> users;
 
     public Role() {}
@@ -24,5 +26,16 @@ public class Role {
     public Role(String name) {
         this.name = name;
     }
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Role)) return false;
+        Role role = (Role) o;
+        return id != null && id.equals(role.getId());
+    }
 
+    @Override
+    public int hashCode() {
+        return getClass().hashCode(); // Hoặc đơn giản là trả về một số hằng số
+    }
 }
